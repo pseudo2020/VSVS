c43eef10ba0afd1a8f3ee8427acd27ce2790ad1d
John Easton Osborne <j.e.osborne2020@gmail.com>
2022-10-18 19:06:46 +0100

    VSVS

---
+++ README.md
@@ -0,0 +1,4 @@
+# VSVS - very simple virus simulation
+
+Models growth of a virus in a patient.
+Does not consider anti-viral drugs taken.
---
+++ main.py
@@ -0,0 +1,12 @@
+import simplev as v
+
+
+def main():
+    sim = v.Simulation()
+    sim.run()
+    sim.plot()
+    return
+
+
+if __name__ == '__main__':
+    main()
